371ab7ed2ba9414a836f2b4e476c137f91b6f87e
David Goez <davidgoez@hotmail.com>
2024-11-25 17:17:48 +0100

    Delete Cuckoo Route planning directory

--- Cuckoo Route planning/Dis_geodesica.m
+++
@@ -1,8 +0,0 @@
-function dist=Dis_geodesica(A,B)
-    a=90-A(1);
-    b=90-B(1);
-    Lambda=A(2)-B(2);
-    angulo_geo=cosd(a)*cosd(b)+sind(a)*sind(b)*cosd(Lambda);
-    Acp=acosd(angulo_geo);
-    dist=Acp*40000/360;
-end
